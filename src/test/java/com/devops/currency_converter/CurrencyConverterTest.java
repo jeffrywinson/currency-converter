d8d98c6e0446c98fca4e2fdd1597176afa04e76e
jeffrywinson <jeffrywinson@gmail.com>
2025-06-17 13:33:14 +0530

    added some JUnit test cases

--- src/test/java/com/devops/currency_converter/CurrencyConverterTest.java
+++ src/test/java/com/devops/currency_converter/CurrencyConverterTest.java
@@ -2,21 +2,29 @@ package com.devops.currency_converter;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class CurrencyConverterTest {
 
+    CurrencyConverter converter = new CurrencyConverter();
+
     @Test
-    public void testValidConversion() {
-        CurrencyConverter converter = new CurrencyConverter();
-        double result = converter.convert("USD", "INR", 1.0);
+    public void testUsdToInr() {
+        double result = converter.convert("USD", "INR", 1);
         assertEquals(83.0, result, 0.01);
     }
 
     @Test
-    public void testInvalidCurrency() {
-        CurrencyConverter converter = new CurrencyConverter();
+    public void testInrToUsd() {
+        double result = converter.convert("INR", "USD", 83);
+        assertEquals(1.0, result, 0.01);
+    }
+
+    @Test
+    void testInvalidCurrency() {
         assertThrows(IllegalArgumentException.class, () -> {
-            converter.convert("ABC", "USD", 100.0);
+            converter.convert("ABC", "INR", 100);
         });
     }
+
 }
